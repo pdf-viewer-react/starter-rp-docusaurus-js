c7b339f3a6d55e7da227fb11554ae7a7f11c25b4
David <david@react-pdf.dev>
2025-09-20 23:03:07 +0800

    Initial commit

---
+++ src/components/AppPdfViewer/index.jsx
@@ -0,0 +1,29 @@
+import {
+  RPDefaultLayout,
+  RPPages,
+  RPProvider,
+} from "@pdf-viewer/react";
+
+const DEFAULT_PDF_URL = 
+  "https://cdn.codewithmosh.com/image/upload/v1721763853/guides/web-roadmap.pdf";
+
+const AppPdfViewer = (props) => {
+  const { showToolbar = true, providerProps, defaultLayoutProps } = props;
+  return (
+    <div>
+      <RPProvider src={DEFAULT_PDF_URL} {...providerProps}>
+        {showToolbar ? (
+          <RPDefaultLayout {...defaultLayoutProps}>
+            <RPPages />
+          </RPDefaultLayout>
+        ) : (
+          <div style={{ width: "100%", height: "550px" }}>
+            <RPPages />
+          </div>
+        )}
+      </RPProvider>
+    </div>
+  );
+};
+
+export default AppPdfViewer;
---
+++ src/pages/index.jsx
@@ -0,0 +1,32 @@
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import AppPdfViewer from "@site/src/components/AppPdfViewer";
+import { RPConfig } from "@pdf-viewer/react";
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <RPConfig>
+      <div className="container">
+        <h1>{siteConfig.title}</h1>
+        <br />
+        <h2>Default Toolbar</h2>
+        <AppPdfViewer />
+        <br />
+        <h2>Without Toolbar</h2>
+        <AppPdfViewer
+          showToolbar={false}
+          defaultLayoutProps={{
+            style: { width: "100%", height: "550px" },
+          }}
+        />
+        <br />
+        <h2>Mobile</h2>
+        <AppPdfViewer
+          defaultLayoutProps={{
+            style: { width: "500px" },
+          }}
+        />
+      </div>
+    </RPConfig>
+  );
+}
